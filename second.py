0eeb095f4dd3bb981566825c72821a6ab776fd7c
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2022-04-09 15:41:37 +0530

    Create second.py

---
+++ second.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+options=Options()
+options.headless=True
+driver = webdriver.Firefox(options=options)
+driver.get('https://www.guvi.in')
+data = [driver.title, driver.current_url]
+print(data)
+driver.quit()
