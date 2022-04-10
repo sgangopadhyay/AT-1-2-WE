285f2487f69befbbe2e2e4eda38cf5b26dbf5e3f
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2022-04-10 15:04:17 +0530

    Update dom.py

--- dom.py
+++ dom.py
@@ -15,9 +15,21 @@ class Suman:
         except:
             print('error:ID not found !')
 
+    def click_link_by_class(self,link_class,url):
+        try:
+            self.driver.get(url)
+            link_class=self.driver.find_element(by=By.CLASS_NAME, value=link_class)
+            if link_class:
+                time.sleep(4)
+                link_class.click()
+        except:
+            print('error:CLASS not found !')
+
 s = Suman()
 
 url = "https://www.w3schools.com/"
 id_1 = "w3loginbtn"
 
-s.click_link_by_id(id_1, url)
+class_1 = "w3-hover-text-green"
+
+s.click_link_by_class(class_1,url)
