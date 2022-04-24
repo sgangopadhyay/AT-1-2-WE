ee6d3512524e83db5aa47c1ac34887fc253d4ec2
Suman Gangopadhyay <linuxgurusuman@gmail.com>
2022-04-24 14:39:01 +0530

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -16,10 +16,33 @@ class Suman:
         time.sleep(2)
         alert_box.accept()
 
+    def confirmatory_alert(self):
+        self.driver.get(self.url)
+        btn2 = self.driver.find_element(by=By.ID, value='btn2')
+        btn2.click()
+        alert_box = self.driver.switch_to.alert
+        time.sleep(3)
+        alert_box.accept()
+        # alert box when cancel is clicked
+        time.sleep(5)
+        self.driver.refresh()
+        btn2 = self.driver.find_element(by=By.ID, value='btn2')
+        btn2.click()
+        alert_box = self.driver.switch_to.alert
+        time.sleep(3)
+        alert_box.dismiss()
 
+    def prompt_box(self):
+        self.driver.get(self.url)
+        btn3 = self.driver.find_element(by=By.ID, value='btn3')
+        btn3.click()
+        alert_box = self.driver.switch_to.alert
+        time.sleep(3)
+        alert_box.send_keys('Suman Gangopadhyay')
+        time.sleep(4)
+        alert_box.accept()
 
 
 s = Suman()
 
-s.simple_alert()
-
+s.prompt_box()
